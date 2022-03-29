b2e94eb66ca5ca29892c3ffd52d06168211c92ae
Selena Nguyen <missselenah@gmail.com>
2022-03-28 18:20:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Login/Logout buttons
+
+function logOut(element) {
+    element.innerText = "Logout";
+}
+
+// Remove Add Definition button on page when clicked
+
+function hide(element) {
+    element.remove();
+}
